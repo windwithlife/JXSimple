f502988aee82cb69a1e88de4b74ecf50c369726a
zhangyq <379163259@qq.com>
2016-08-23 17:41:18 +0800

    solved IM websocket bind

--- simple/simple-base/src/main/java/com/simple/base/components/nio/entity/RegisterDeviceRequest.java
+++ simple/simple-base/src/main/java/com/simple/base/components/nio/entity/RegisterDeviceRequest.java
@@ -2,7 +2,7 @@ package com.simple.base.components.nio.entity;
 
 public class RegisterDeviceRequest {
 	public int    command  = 10001;
-	public String deviceId = "123456";
+	public String deviceCode = "123456";
 	public String deviceType = "air-conditioner";
 	
 }
--- simple/simple-base/src/main/java/com/simple/base/components/nio/service/DeviceService.java
+++ simple/simple-base/src/main/java/com/simple/base/components/nio/service/DeviceService.java
@@ -1,12 +1,12 @@
 package com.simple.base.components.nio.service;
 
-import org.springframework.stereotype.Service;
+//import org.springframework.stereotype.Service;
 
 import com.simple.base.components.nio.entity.RegisterDeviceRequest;
 import com.simple.base.components.nio.entity.RegisterDeviceResponse;
-import com.simple.base.components.nio.framework.ConnectionManager;
-import com.simple.base.components.nio.framework.IConnection;
-import com.simple.base.components.nio.framework.Request;
+//import com.simple.base.components.nio.framework.ConnectionManager;
+//import com.simple.base.components.nio.framework.IConnection;
+//import com.simple.base.components.nio.framework.Request;
 import com.simple.base.components.nio.framework.RequestContext;
 
 
@@ -17,7 +17,7 @@ public class DeviceService {
 	
 	@HandlerMapping(id="10001", path="registerDevice")
 	public  void registerDevice(RegisterDeviceRequest request, RegisterDeviceResponse response,RequestContext context){
-		System.out.println("register device type is:" + request.deviceType + " id:" + request.deviceId);
+		System.out.println("register device type is:" + request.deviceType + " id:" + request.deviceCode);
         context.getResponse().doResponse(response);
 	}
 
