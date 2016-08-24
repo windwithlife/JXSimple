ebc3fb0d31b1286b2a5cae7cb6dee5477abbcb7e
zhangyq <379163259@qq.com>
2016-08-24 17:02:01 +0800

    add log file configuration

--- simple/simple-base/src/main/java/com/simple/base/components/user/config/WebSecurityConfig.java
+++ simple/simple-base/src/main/java/com/simple/base/components/user/config/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.authorizeRequests()
 			.antMatchers("/css/**", "/index","/img","/js").permitAll()
 			.antMatchers("/admin/**").authenticated();
-		http.formLogin().loginPage("/login").failureUrl("/test");
+		http.formLogin().loginPage("/login").failureUrl("/error");
 		http.logout().permitAll();
 		http.csrf().disable(); //permist csrf.
 		http.headers().frameOptions().sameOrigin();//Permit cross origin frame source display.
