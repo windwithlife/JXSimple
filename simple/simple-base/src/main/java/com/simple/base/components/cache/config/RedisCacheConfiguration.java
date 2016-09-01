2bcc9366ca5b4769e98ead3417df4bb4f254b2c7
zhangyq <379163259@qq.com>
2016-09-01 16:53:02 +0800

    support docker and daocloud

--- simple/simple-base/src/main/java/com/simple/base/components/cache/config/RedisCacheConfiguration.java
+++ simple/simple-base/src/main/java/com/simple/base/components/cache/config/RedisCacheConfiguration.java
@@ -23,21 +23,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @EnableCaching//
 public class RedisCacheConfiguration extends CachingConfigurerSupport {
    
-    /**
-     * 缂撳瓨绠＄悊鍣�.
-     * @param redisTemplate
-     * @return
-     */
+ 
     @Bean
     public CacheManager cacheManager(RedisTemplate<?,?> redisTemplate) {
        CacheManager cacheManager = new RedisCacheManager(redisTemplate);
        return cacheManager;
     }
  
-    /**
-     * 鑷畾涔塳ey.
-     * 姝ゆ柟娉曞皢浼氭牴鎹被鍚�+鏂规硶鍚�+鎵�鏈夊弬鏁扮殑鍊肩敓鎴愬敮涓�鐨勪竴涓猭ey,鍗充娇@Cacheable涓殑value灞炴�т竴鏍凤紝key涔熶細涓嶄竴鏍枫��
-     */
     @Override
     public KeyGenerator keyGenerator() {
        System.out.println("RedisCacheConfig.keyGenerator()");
@@ -57,27 +49,17 @@ public class RedisCacheConfiguration extends CachingConfigurerSupport {
            }
        };
     }
-    /**
-     * RedisTemplate缂撳瓨鎿嶄綔绫�,绫讳技浜巎dbcTemplate鐨勪竴涓被;
-     *
-     * 铏界劧CacheManager涔熻兘鑾峰彇鍒癈ache瀵硅薄锛屼絾鏄搷浣滆捣鏉ユ病鏈夐偅涔堢伒娲伙紱
-     *
-     * 杩欓噷鍦ㄦ墿灞曚笅锛歊edisTemplate杩欎釜绫讳笉瑙佸緱寰堝ソ鎿嶄綔锛屾垜浠彲浠ュ湪杩涜鎵╁睍涓�涓垜浠�
-     *
-     * 鑷繁鐨勭紦瀛樼被锛屾瘮濡傦細RedisStorage绫�;
-     *
-     * @param factory : 閫氳繃Spring杩涜娉ㄥ叆锛屽弬鏁板湪application.properties杩涜閰嶇疆锛�
-     * @return
-     */
+   
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
        RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
        redisTemplate.setConnectionFactory(factory);
       
       
-       RedisSerializer<String> redisSerializer = new StringRedisSerializer();//Long绫诲瀷涓嶅彲浠ヤ細鍑虹幇寮傚父淇℃伅;
+       RedisSerializer<String> redisSerializer = new StringRedisSerializer();
        redisTemplate.setKeySerializer(redisSerializer);
        redisTemplate.setHashKeySerializer(redisSerializer);
+       redisTemplate.setValueSerializer(redisSerializer);
       
        return redisTemplate;
     }
